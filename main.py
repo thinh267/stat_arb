41363d22be2d4333375777a552049b8397383d5d
thinh267 <tienthinh2607.gpa@gmail.com>
2025-07-18 10:50:42 +0700

    finalll

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# main.py
-from core.signal_generator import test_save_signal_to_db
-from scheduler.scheduler import run_scheduler,signal_task
+from scheduler.scheduler import run_scheduler
 if __name__ == "__main__":  
-    # signal_task()
-    # run_scheduler()   
-    test_save_signal_to_db()
+    run_scheduler()
