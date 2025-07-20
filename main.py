831369534ab5f6b4af17eb7df8c80f2da8b777af
thinh267 <tienthinh2607.gpa@gmail.com>
2025-07-20 16:04:12 +0700

    shibaaa

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-from scheduler.scheduler import run_scheduler
-if __name__ == "__main__":  
-    run_scheduler()
-=======
+
 # main.py
 from scheduler.scheduler import run_scheduler
 if __name__ == "__main__":  
     run_scheduler()   
-
->>>>>>> b0aedc77b27cd4cb90d3c11b4d5a98232a6ab962
